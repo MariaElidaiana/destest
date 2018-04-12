69c7c86a2e1eaa9cbc39c0c138688289942a34d7
Maria Elidaiana <elidaiana.sp@gmail.com>
2018-04-12 11:27:44 -0500

    fixing the logic of subdir creation in file_path

--- destest.py
+++ destest.py
@@ -67,8 +67,9 @@ def file_path( params, subdir, name, var=None, var2=None, var3=None, ftype='txt'
             raise IOError('Output directory already exists. Set output_exists to True to use existing output directory at your own peril.')
     else:
         if not os.path.exists(fpath):
+       	    os.mkdir(fpath)
+        if not os.path.exists(os.path.join(params['output'],params['param_file'][:params['param_file'].index('.')])) :
             os.mkdir(os.path.join(params['output'],params['param_file'][:params['param_file'].index('.')]))
-        os.mkdir(fpath)
         params['output_exists'] = True
 
     return os.path.join(fpath,name)
